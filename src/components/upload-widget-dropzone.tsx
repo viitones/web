382b805a77e8ebd366a381dc23c738ecf4e4e1b2
viitones <vitonesmartins@gmail.com>
2025-12-12 20:28:11 -0300

    dropzone e titulo

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -1,6 +1,31 @@
+import { useDropzone } from "react-dropzone"
+
 export function UploadWidgetDropzone() {
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({
+    multiple: true,
+    accept: {
+      'image/png': ['.png'],
+      'image/jpeg': ['.jpeg', '.jpg'],
+    },
+    onDrop(acceptedFiles, fileRejections, event) {
+      console.log({acceptedFiles, fileRejections, event})
+    },
+  })
+
   return (
-    <div className="px-3 h-32"></div>
+    <div className="px-3 flex flex-col gap-3">
+      <div
+        data-active={isDragActive}
+        className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500 data-[active=true]:text-indigo-400 justify-center "
+      {...getRootProps()} >
+        <input type="file" {...getInputProps()} />
+
+        <span className="text-xs">Drop your files here or</span>
+        <span className="text-xs underline">Click to open file picker</span>
+      </div>
+
+        <span className="text-xs text-zinc-400">Only PNG and JPEG files are supported</span>
+    </div>
     
   )
 }
--- src/components/upload-widget-header.tsx
+++ src/components/upload-widget-header.tsx
@@ -1,11 +1,12 @@
 import * as Collapsible from "@radix-ui/react-collapsible";
 
 import { Minimize2 } from "lucide-react";
+import { UploadWidgetTitle } from "./upload-widget-title";
 
 export function UploadWidgetHeader() {
   return (
     <div className="w-full p-4 py-2 bg-white/2 border-zinc-800 border-b flex items-center justify-between">
-      <span className="text-sm font-medium">Upload files</span>
+      <UploadWidgetTitle />
 
       <Collapsible.Trigger asChild>
         <Minimize2 strokeWidth={1.5} className="size-4" />
