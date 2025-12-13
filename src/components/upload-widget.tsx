a9ee14197eccca93bd55d2e0ec277c457a41cea2
viitones <vitonesmartins@gmail.com>
2025-12-13 01:30:36 -0300

    solucionando bug classes tailwind

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -13,7 +13,7 @@ export function UploadWidget() {
     <Collapsible.Root asChild onOpenChange={() => toggleWidgetOpen()}>
       <motion.div
         data-progress={isThereAnyPendingUploads}
-        className="bg-zinc-900 overflow-hidden max-w-90 rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,theme(colors.zinc.900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.zinc.700/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.zinc.600/.48))_border-box]"
+        className="bg-zinc-900 overflow-hidden max-w-90 rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,--theme(--color-zinc-900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),--theme(--color-zinc-700/.48)_80%,--theme(--color-indigo-500)_86%,--theme(--color-indigo-300)_90%,--theme(--color-indigo-500)_94%,--theme(--color-zinc-600/.48))_border-box]"
         // initial={{ width: "max-content", height: 44 }}
         animate={ isWidgetOpen ? { width: 320, height: "auto" } : { width: "max-content", height: 44 } }
         transition={{ duration: 0.2 }}
