1f3c8e971c2ca64e974a55a5d3c9011cdc96155c
viitones <vitonesmartins@gmail.com>
2025-12-13 00:50:36 -0300

    borda gradiente no botão

--- src/components/upload-widget-upload-item.tsx
+++ src/components/upload-widget-upload-item.tsx
@@ -1,14 +1,14 @@
-import {motion} from "motion/react"
 import * as Progress from "@radix-ui/react-progress";
 import { Download, ImageUp, Link2, RefreshCcw, X } from "lucide-react";
+import { motion } from "motion/react";
 import { Button } from "./ui/button";
 
 export function UploadWidgetUploadItem() {
   return (
-    <motion.div 
-      initial={{opacity: 0}}
-      animate={{opacity: 1}}
-      transition={{duration: 0.25}}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
       className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden"
     >
       <div className="flex flex-col gap-1">
@@ -30,7 +30,10 @@ export function UploadWidgetUploadItem() {
       </div>
 
       <Progress.Root className="bg-zinc-800 rounded-full h-1 overflow-hidden">
-        <Progress.Indicator className="bg-indigo-500 h-1" style={{width: '43%'}}/>
+        <Progress.Indicator
+          className="bg-indigo-500 h-1"
+          style={{ width: "43%" }}
+        />
       </Progress.Root>
 
       <div className="absolute top-2.5 right-2.5 flex items-center gap-2.5">
--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -6,29 +6,17 @@ import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
 
 export function UploadWidget() {
+  const isThereAnyPendingUploads = true;
   const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
 
   return (
-    <Collapsible.Root onOpenChange={() => toggleWidgetOpen()}>
+    <Collapsible.Root asChild onOpenChange={() => toggleWidgetOpen()}>
       <motion.div
-        animate={isWidgetOpen ? "open" : "closed"}
-        variants={{
-          closed: {
-            width: "max-content",
-            height: 44,
-            transition: {
-              type: "inertia",
-            }
-          },
-          open: {
-            width: 360,
-            height: "auto",
-            transition: {
-              duration: 0.1,
-            }
-          },
-        }}
-        className="bg-zinc-900 overflow-hidden max-w-90 rounded-xl shadow-shape"
+        data-progress={isThereAnyPendingUploads}
+        className="bg-zinc-900 overflow-hidden max-w-90 rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,theme(colors.zinc.900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.zinc.700/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.zinc.600/.48))_border-box]"
+        // initial={{ width: "max-content", height: 44 }}
+        animate={ isWidgetOpen ? { width: 320, height: "auto" } : { width: "max-content", height: 44 } }
+        transition={{ duration: 0.2 }}
       >
         {!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
